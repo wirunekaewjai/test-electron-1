f6c96d0e8830e140ebbed66a9d5f489b4653c3d2
Wirune Kaewjai <wirune.kae@hotmail.com>
2021-01-04 22:22:34 +0700

    ทดลองใช้ preact ทำ class component และ function component + hook (useState)

--- learn-1/src/pages/contact.ts
+++ learn-1/src/pages/contact.ts
@@ -1,11 +1,25 @@
 import { html } from 'htm/preact';
+import { useState } from 'preact/hooks';
 
 export default function Page ()
 {
+  const [count, setCount] = useState(0);
+
+  function onClick ()
+  {
+    setCount(e => e + 1);
+  }
+
   return html`
   <h1>
     # Contact
   </h1>
+  <button onclick="${onClick}" >
+    CLICK ME !!!
+  </button>
+  <pre>
+    Count: ${count}
+  </pre>
   <ul>
     <li>
       <a href="/" >
--- learn-1/src/pages/home.ts
+++ learn-1/src/pages/home.ts
@@ -1,22 +1,59 @@
-import { html } from 'htm/preact';
+import { html, Component } from 'htm/preact';
 
-export default function Page ()
-{
-  return html`
-  <h1>
-    # Index
-  </h1>
-  <ul>
-    <li>
-      <a href="/contact" >
-        Contact
-      </a>
-    </li>
-    <li>
-      <a href="/about" >
-        About
-      </a>
-    </li>
-  </ul>
-  `;
+export default class Page extends Component {
+  timer: NodeJS.Timeout = undefined;
+  state: {
+    count: number;
+    time: number;
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+      time: Date.now(),
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  onClick () {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render () {
+    const time = new Date(this.state.time).toLocaleTimeString();
+
+    return html`
+    <h1>
+      # Index ${time}
+    </h1>
+    <button onclick="${this.onClick}" >
+      ${this.state.count}
+    </button>
+    <ul>
+      <li>
+        <a href="/contact" >
+          Contact
+        </a>
+      </li>
+      <li>
+        <a href="/about" >
+          About
+        </a>
+      </li>
+    </ul>
+    `;
+  }
 }
