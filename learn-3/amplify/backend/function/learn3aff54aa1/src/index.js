618de3ef4b77fda04220a45a93745d71efbbb067
Wirune Kaewjai <wirune.kae@hotmail.com>
2021-01-12 19:42:51 +0700

    สำเร็จการ query เพื่อ sum amount of transactions

--- learn-3/amplify/backend/function/learn3aff54aa1/src/index.js
+++ learn-3/amplify/backend/function/learn3aff54aa1/src/index.js
@@ -1,15 +1,69 @@
+/* Amplify Params - DO NOT EDIT
+  API_LEARN3_GRAPHQLAPIENDPOINTOUTPUT
+  API_LEARN3_GRAPHQLAPIIDOUTPUT
+  ENV
+  REGION
+Amplify Params - DO NOT EDIT */
+const axios = require('axios');
+const gql = require('graphql-tag');
+const graphql = require('graphql');
+const { print } = graphql;
 
+// const total = gql`
+//   query Total($walletID: ID!) {
+//     total(walletID: $walletID)
+//   }
+// `;
+const listTransactions = gql`
+  query ListTransactions ($walletID: ID!) {
+    listTransactions(filter: {walletID: {eq: $walletID}}) {
+      startedAt
+      items {
+        amount
+      }
+    }
+  }
+`;
 
-exports.handler = async (event) => {
-    // TODO implement
-    // const response = {
-    //     statusCode: 200,
-    // //  Uncomment below to enable CORS requests
-    // //  headers: {
-    // //      "Access-Control-Allow-Origin": "*",
-    // //      "Access-Control-Allow-Headers": "*"
-    // //  }, 
-    //     body: JSON.stringify('Hello from Lambda!'),
-    // };
-    return 1234;
+exports.handler = async (event) =>
+{
+  // console.log(JSON.stringify(event, null, 2));
+  // console.log(JSON.stringify(process.env, null, 2));
+
+  // return 1234;
+
+  const walletID = event.arguments.walletID;
+  
+  try {
+    const result = await axios.post(
+      process.env.API_LEARN3_GRAPHQLAPIENDPOINTOUTPUT,
+      {
+        query: print(listTransactions),
+        variables: {
+          walletID,
+        },
+      },
+      {
+        headers: {
+          // 'x-api-key': process.env.API_LEARN3_GRAPHQLAPIIDOUTPUT,
+          'authorization': event.request.headers.authorization,
+        },
+      }
+    );
+
+    const transactions = result.data.data.listTransactions.items;
+    
+    let sum = 0;
+
+    for (const transaction of transactions) {
+      sum += transaction.amount;
+    }
+
+    return sum;
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  return Number.MIN_SAFE_INTEGER;
 };
